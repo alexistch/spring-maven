cabbadf8c6a11129efd68bb883e5b3071647dd5e
unknown <alexis.tchoulev@efrei.net>
2025-04-01 15:40:12 +0200

    COMMIT AVEC HELLOWORLD

--- demo-github/src/main/java/com/example/demo_github/DemoGithubApplication.java
+++ demo-github/src/main/java/com/example/demo_github/DemoGithubApplication.java
@@ -1,13 +1,18 @@
-package com.example.demo_github;
-
+package com.example.dockerdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
-public class DemoGithubApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoGithubApplication.class, args);
-	}
-
+public class DockerDemoApplication {
+ public static void main(String[] args) {
+ SpringApplication.run(DockerDemoApplication.class, args);
+ }
+}
+@RestController
+class HelloWorldController {
+ @GetMapping("/")
+ public String sayHello() {
+ return "Hello, World! (ALEXIS TCHOULEV)";
+ }
 }
